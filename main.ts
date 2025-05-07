9ee6c5e4674b134308abc253af82e5f6b41ca069
De-sir-e <desireelopez078@gmail.com>
2025-05-07 18:18:12 -0400

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
-    mySprite,
+    myMiner,
     assets.animation`Walk backward`,
     100,
     false
@@ -8,7 +8,7 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
-    mySprite,
+    myMiner,
     assets.animation`Walk Left`,
     150,
     false
@@ -16,7 +16,7 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
-    mySprite,
+    myMiner,
     assets.animation`Walk Right`,
     100,
     false
@@ -24,22 +24,25 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
-    mySprite,
+    myMiner,
     assets.animation`Walk forward`,
     100,
     false
     )
 })
-let mySprite: Sprite = null
-mySprite = sprites.create(assets.image`myImage9`, SpriteKind.Player)
+let myMiner: Sprite = null
+tiles.setCurrentTilemap(tilemap`level 1`)
+myMiner = sprites.create(assets.image`Miner`, SpriteKind.Player)
 let mySprite2 = sprites.create(assets.image`Slime 2`, SpriteKind.Enemy)
+scene.cameraFollowSprite(myMiner)
+myMiner.setPosition(176, 74)
+mySprite2.setPosition(144, 464)
 animation.runImageAnimation(
 mySprite2,
 assets.animation`Slime Animation`,
 100,
 true
 )
-controller.moveSprite(mySprite, 100, 100)
-mySprite.setPosition(77, 55)
+controller.moveSprite(myMiner, 100, 100)
+myMiner.setPosition(77, 55)
 mySprite2.setBounceOnWall(true)
-mySprite2.follow(mySprite, 10)
