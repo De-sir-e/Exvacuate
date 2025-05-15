2f3a708b6dea67393e11e9af809787f851195619
De-sir-e <desireelopez078@gmail.com>
2025-05-14 21:48:45 -0400

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,23 @@
+namespace SpriteKind {
+    export const Object = SpriteKind.create()
+}
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
     myMiner,
     assets.animation`Walk backward`,
     100,
-    false
+    true
     )
+})
+controller.anyButton.onEvent(ControllerButtonEvent.Released, function () {
+    animation.stopAnimation(animation.AnimationTypes.All, myMiner)
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
     myMiner,
     assets.animation`Walk Left`,
     150,
-    false
+    true
     )
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -19,7 +25,7 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     myMiner,
     assets.animation`Walk Right`,
     100,
-    false
+    true
     )
 })
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -27,16 +33,18 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     myMiner,
     assets.animation`Walk forward`,
     100,
-    false
+    true
     )
 })
 let myMiner: Sprite = null
-tiles.setCurrentTilemap(tilemap`level 1`)
+tiles.setCurrentTilemap(tilemap`level 1 basic`)
 myMiner = sprites.create(assets.image`Miner`, SpriteKind.Player)
 let mySprite2 = sprites.create(assets.image`Slime 2`, SpriteKind.Enemy)
+let mySprite3 = sprites.create(assets.image`Lantern`, SpriteKind.Object)
 scene.cameraFollowSprite(myMiner)
 myMiner.setPosition(176, 74)
 mySprite2.setPosition(144, 464)
+mySprite3.setPosition(106, 72)
 animation.runImageAnimation(
 mySprite2,
 assets.animation`Slime Animation`,
